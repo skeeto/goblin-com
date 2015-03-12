8f1e3a4ca2d2eb5543b08e3a8257acc830844ba5
Christopher Wellons <wellons@nullprogram.com>
2015-03-11 21:14:32 -0400

    Add a random name generator.

--- rand.c
+++ rand.c
@@ -1,4 +1,6 @@
 #include <string.h>
+#include <ctype.h>
+#include <math.h>
 #include "rand.h"
 
 #define MIN(x, y) ((y) < (x) ? (y) : (x))
@@ -40,4 +42,44 @@ rand_uniform(float min, float max)
 {
     return rand_uniform_s(&rand_state, min, max);
 
+}
+
+int
+rand_range_s(uint64_t *state, int min, int max)
+{
+    uint64_t x = xorshift(state);
+    return (x % (max - min)) + min;
+}
+
+int
+rand_range(int min, int max)
+{
+    return rand_range_s(&rand_state, min, max);
+}
+
+void
+rand_name(char *name, size_t max)
+{
+    /* This thing sucks ... */
+    const char *vowels = "aeiou";
+    const char *consonants = "bcdfghjklmnpqrstvwxyz";
+    int length = rand_range(6, max);
+    int spaces = 0;
+    int letters = 0;
+    for (int i = 0; i < length; i++) {
+        float spacep = letters / 3.0f * powf(0.1f, 1.0f / (10 * spaces + 1));
+        if (i > 0 && rand_uniform(0, 1.0) < spacep) {
+            name[i] = ' ';
+            letters = 0;
+        } else {
+            if (i % 2)
+                name[i] = consonants[rand_range(0, strlen(consonants))];
+            else
+                name[i] = vowels[rand_range(0, strlen(vowels))];
+            if (letters == 0)
+                name[i] = toupper((int)name[i]);
+            letters++;
+        }
+    }
+    name[length] = '\0';
 }
--- rand.h
+++ rand.h
@@ -13,3 +13,8 @@ void     xorshift_fill(uint64_t *state, void *, size_t);
 
 float rand_uniform_s(uint64_t *state, float min, float max);
 float rand_uniform(float min, float max);
+
+int rand_range_s(uint64_t *state, int min, int max);
+int rand_range(int min, int max);
+
+void rand_name(char *name, size_t max);
